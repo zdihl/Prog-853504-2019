03907460d3828878440fa66b4c6af40c1e958531
JailSeed <45213700+JailSeed@users.noreply.github.com>
2019-03-01 22:59:16 +0300

    lab1.c

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -4,29 +4,23 @@
 
 int main()
 {
-	int i,j,chislo;
-	double ch[100],sum;
-	sum=0;
-	ch[0]=0;
-	ch[1]=1;
-	for (i=2; i<100; i++) {
-		ch[i]=ch[i-1]+ch[i-2];
-		if (ch[i]>INT_MAX) {
-			i--;
-			chislo=ch[i];
-			printf("Nomer poslednego chisla: %d %s",i,"\n");
-			printf("Poslednee chislo: %d %s",chislo,"\n");
-			for (j=0; sum<INT_MAX; j++) {
-				sum=sum+ch[j];
-			}
-			if (sum>INT_MAX) {
-				sum=sum-ch[i-1];
-			}
-			printf("Summa chisel: %.0f %s",sum,"\n");
-			goto exit;
+	int prevnum, num, sum, counter, mem;
+	prevnum=1;
+	num=1;
+	counter=2;
+	sum=2;
+	while ((unsigned long)prevnum+num<=INT_MAX) {
+		mem=num;
+		num+=prevnum;
+		prevnum=mem;
+		counter++;
+		if ((unsigned long)sum+num<=INT_MAX) {
+			sum+=num;
 		}
 	}
-	exit:
+	printf("Nomer poslednego chisla: %d %s",counter,"\n");
+	printf("Poslednee chislo: %d %s",num,"\n");
+        printf("Summa chisel: %d",sum,"\n");
 	getch();
 	return 0;
 }
