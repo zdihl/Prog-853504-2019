aa0c71c719dbfce6bda12b2108550e8916fc22d1
Nikita Shepshuk <nikitashepshuk@gmail.com>
2019-03-03 13:31:15 +0300

    Added some comments

--- shepshook/lab1.c
+++ shepshook/lab1.c
@@ -2,11 +2,13 @@
 #include <limits.h>
 
 int main() {
+
 	int fib0 = 1;
 	int fib1 = 1;
 	int fib2;
 	int i = 2;
-	while (fib1 < UCHAR_MAX) {
+
+	while (fib1 < UCHAR_MAX) { //Searching for maximum fib number fitting in Byte
 		fib2 = fib1 + fib0;
 		fib0 = fib1;
 		fib1 = fib2;
@@ -17,7 +19,7 @@ int main() {
 	long long mult = 1;
 	fib0 = 1;
 	fib1 = 1;
-	while (mult < INT_MAX) {
+	while (mult < INT_MAX) { //Searching for maximum fib numbers multiplication fitting in Int
 		fib2 = fib1 + fib0;
 		mult *= fib2;
 
@@ -25,7 +27,7 @@ int main() {
 		fib1 = fib2;
 		i++;
 	}
-	printf("Maximum multiplication: %lld\n", mult / fib2);
+	printf("Maximum multiplication: %lld\n", mult / fib2); //Dividing by the last fib number to make (mult < INT_MAX)
 
 	return 0;
 }
