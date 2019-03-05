76929b9888a483c5fe9ac699ed41ca41c71ff752
CyrilAndreyev <47887536+CyrilAndreyev@users.noreply.github.com>
2019-03-05 12:29:53 +0300

    [Андреев Кирилл] lab#1 variant-#1
    С клавиатуры вводится целое число х из промежутка [100, 
    9999]. Если число четырехзначное, то найти сумму его цифр, а 
    если трехзначное, то произведение цифр числа.

---
+++ CyrilAndreyev/lab1.c
@@ -0,0 +1,44 @@
+/* Variant 1 */
+
+#pragma hdrstop
+#pragma argsused
+
+#include <stdio.h>
+#include <conio.h>
+
+#ifdef _WIN32
+#include <tchar.h>
+#else
+  typedef char _TCHAR;
+  #define _tmain main
+#endif
+
+int _tmain(int argc, _TCHAR* argv[]) 
+{
+	long long int x;
+	printf("x = ");
+	scanf_s("%lli", &x);
+	if ((x < 100) || (x > 9999)) {
+		printf("Invalid input!");
+		getch();
+		return 0;
+	}
+	if (x / 1000) {
+		short int sum = 0;
+		while (x) {
+			sum += x % 10;
+			x /= 10;
+		}
+		printf("%hi", sum);
+	}
+	else {
+		short int pr = 1;
+		while (x) {
+			pr *= x % 10;
+			x /= 10;
+		}
+		printf("%hi", pr);
+	}
+    getch();
+	return 0;
+}
